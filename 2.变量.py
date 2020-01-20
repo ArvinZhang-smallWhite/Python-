1ddd5d47b56c202055e67112f9f0567161d37661
ArvinZhang-smallWhite <60085596+ArvinZhang-smallWhite@users.noreply.github.com>
2020-01-20 14:52:06 +0800

    Create 2.变量.py

---
+++ 2.变量.py
@@ -0,0 +1,20 @@
+# 1.输出变量
+num=input("请猜一个数：")
+print("你的输入是："+num)
+
+# 2.变量计算
+money=20
+result=money*2+50
+print(result)
+
+# 3.变量类型
+a = 'hello,world'
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+print(a)#hello,world
+print(b)#1.414
+print(c)#True
+print(d)#False
+print(e)#True
